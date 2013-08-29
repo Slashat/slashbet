580420ce4326f8be8e63d7b51da24f7bea740483
Johan Stenehall <stenehall@gmail.com>
2013-08-30 00:01:41 +0200

    Moving it all to lib

--- app.js
+++ app.js
@@ -3,42 +3,39 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
-var path = require('path');
-
 
 var app = express();
-
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
 
 app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+// development only
+app.configure('development', function(){
+  app.use(express.logger('dev'));
+  app.use(express.errorHandler({showStack: true, dumpExceptions: true}));
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
 var auth = require('./lib/auth');
 app.use(auth);
 
-app.use(express.static(__dirname + '/public'));
-app.use(app.router);
+var main = require('./lib/main');
+app.use(main);
 
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-
-app.get('/', routes.index);
-app.post('/post', auth.ensureAuthenticated, routes.post);
-app.get('/:id', routes.single);
-app.get('/:id/:vote', routes.vote);
 
 
 http.createServer(app).listen(app.get('port'), function(){
