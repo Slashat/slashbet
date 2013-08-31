ace65eb42155be6a8181f0bb904900c999dff323
Johan Stenehall <stenehall@gmail.com>
2013-08-31 21:12:17 +0200

    Adding some tests and moving out db calls to models

--- app.js
+++ app.js
@@ -36,9 +36,6 @@ app.use(auth);
 var main = require('./lib/main');
 app.use(main);
 
-
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
