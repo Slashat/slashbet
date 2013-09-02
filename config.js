fabae4a89884d2e7c15509ee429c653adc32e355
Johan Stenehall <stenehall@gmail.com>
2013-09-02 23:31:15 +0200

    Adding some more tests and cleaning up a bit

--- config.js
+++ config.js
@@ -1,3 +1,14 @@
+var test = {
+  database_url: 'postgres://stenehall:@127.0.0.1/slashbet_test',
+  auth: {
+    github: {
+      clientID: '61f1b46a2a920718343e',
+      clientSecret : '82b89a3d2c5256c88e53d62fde7febe93e8c971c',
+      clientCallback : "http://0.0.0.0:3000/auth/github/callback"
+    }
+  }
+};
+
 var development = {
   database_url: process.env.DATABASE_URL,
   auth: {
@@ -20,4 +31,16 @@ var production = {
   }
 };
 
-exports.Config = global.process.env.NODE_ENV === 'production' ? production : development;
+switch (global.process.env.NODE_ENV) {
+  case 'production':
+    exports.Config = production;
+    break;
+  case 'test':
+    exports.Config = test;
+    break;
+  default:
+    exports.Config = development;
+    break;
+}
+
+//exports.Config = global.process.env.NODE_ENV === 'production' ? production : development;
